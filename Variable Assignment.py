6b947ac4e2a6b6fba4d42df45ce300e635e1d686
eoinmonaghan <emonaghan93@gmail.com>
2020-12-13 22:05:58 +0000

    Variable assignment

--- Variable Assignment.py
+++ Variable Assignment.py
@@ -3,3 +3,15 @@ savings = 100
 
 # Print out savings
 print(savings)
+
+# Create a variable savings
+savings = 100
+
+# Create a variable growth_multiplier
+growth_multiplier = 1.1
+
+# Calculate result
+result = savings*growth_multiplier**7
+
+# Print out result
+print(result)
